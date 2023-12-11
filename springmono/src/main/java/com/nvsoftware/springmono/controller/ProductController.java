7b022f3ee222dd6c49ef19428d642d364564b41e
JRunust <hr574177@gmail.com>
2023-12-11 01:05:54 -0800

    add product crud

--- springmono/src/main/java/com/nvsoftware/springmono/controller/ProductController.java
+++ springmono/src/main/java/com/nvsoftware/springmono/controller/ProductController.java
@@ -3,18 +3,29 @@ package com.nvsoftware.springmono.controller;
 import com.nvsoftware.springmono.model.Product;
 import com.nvsoftware.springmono.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
     @Autowired
     ProductService productService;
-    @PostMapping("/save")
+    @PostMapping()
     public Product save(@RequestBody Product product) {
         return productService.save(product);
+    }
+    @GetMapping
+    public List<Product> getProducts() {
+        return productService.getAll();
+    }
+    @GetMapping("/{id}")
+    public Product getProductByProductId(@PathVariable String id) {
+        return productService.getById(id);
+    }
+    @DeleteMapping("/{id}")
+    public String deleteProductById(@PathVariable String id) {
+        return productService.deleteById(id);
     }
 }
--- springmono/src/main/java/com/nvsoftware/springmono/service/ProductService.java
+++ springmono/src/main/java/com/nvsoftware/springmono/service/ProductService.java
@@ -2,6 +2,11 @@ package com.nvsoftware.springmono.service;
 
 import com.nvsoftware.springmono.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
     Product save(Product product);
+    Product getById(String id);
+    List<Product> getAll();
+    String deleteById(String id);
 }
