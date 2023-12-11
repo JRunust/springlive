fa3f45220f2a270588accff04b044908234f67e5
JRunust <hr574177@gmail.com>
2023-12-10 21:30:15 -0800

    springone setup

---
+++ springmono/src/main/java/com/nvsoftware/springmono/SpringmonoApplication.java
@@ -0,0 +1,13 @@
+package com.nvsoftware.springmono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmonoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmonoApplication.class, args);
+	}
+
+}
