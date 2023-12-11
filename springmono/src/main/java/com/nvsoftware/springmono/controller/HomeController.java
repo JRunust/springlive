f8dd93e13ce92ab8b6830bdd2c8a106539ad8ea0
JRunust <hr574177@gmail.com>
2023-12-10 23:46:10 -0800

    add User model, refactor to restcontroller, setup postman to test api

--- springmono/src/main/java/com/nvsoftware/springmono/controller/HomeController.java
+++ springmono/src/main/java/com/nvsoftware/springmono/controller/HomeController.java
@@ -1,14 +1,23 @@
 package com.nvsoftware.springmono.controller;
 
+import com.nvsoftware.springmono.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@ResponseBody
+@RestController
 public class HomeController {
     @RequestMapping("/")
     public String home(){
         return "NVsoftware home";
+    }
+    @RequestMapping("/user")
+    public User user(){
+        User user = new User();
+        user.setId("31001");
+        user.setEmail("user@nvsoftware.com");
+        user.setName("nvsoftware");
+        return user;
     }
 }
